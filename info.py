845fe8178551cbf51e26ae452c829a8c99945eae
Dacchu38 <vd383846@gmail.com>
2026-03-02 08:24:34 +0530

    Update environment variables for bot configuration

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def str_to_bool(val):
 
 API_ID = int(environ.get('API_ID', '29388536'))
 API_HASH = environ.get('API_HASH', '1795cfeb72fdd9741bbaab5e02c57668')
-BOT_TOKEN = environ.get("BOT_TOKEN", "7436884583:AAEhOx1HzTIgFLA8zLBoi86sizc5VShu6IQ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8603670245:AAEKVivPfBB6rKwSJzPdjAT_QCIhUg0ayuU")
 PORT = environ.get("PORT", "8080")
-START_PIC = environ.get("START_PIC", "https://o.uguu.se/eKNoswZZ.jpg")
+START_PIC = environ.get("START_PIC", "https://files.catbox.moe/045zcm.jpg")
 
 # ── ᴀᴠ ʙᴏᴛᴢ ─────────────────────────────
 # ᴜᴘᴅᴀᴛᴇs  : https://t.me/AV_BOTz_UPDATE
@@ -24,7 +24,7 @@ START_PIC = environ.get("START_PIC", "https://o.uguu.se/eKNoswZZ.jpg")
 # ────────────────────────────────────────
 
 ADMINS = list(map(int, environ.get("ADMINS", "5912466219").split()))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003571958820"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003888435322"))
 DB_URL = environ.get('DATABASE_URI', "mongodb+srv://rai:rai@cluster0.fktwown.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # ── ᴀᴠ ʙᴏᴛᴢ ─────────────────────────────
@@ -34,10 +34,10 @@ DB_URL = environ.get('DATABASE_URI', "mongodb+srv://rai:rai@cluster0.fktwown.mon
 # ᴄʀᴇᴅɪᴛ   : ᴀᴠ ʙᴏᴛᴢ | ᴀᴍᴀɴ ᴠɪsʜᴡᴀᴋᴀʀᴍᴀ
 # ────────────────────────────────────────
 
-AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1003571958820").split()))
-AUTH_REQ_CHANNEL = list(map(int, environ.get("AUTH_REQ_CHANNELS", "-1003571958820").split()))
+AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "").split()))
+AUTH_REQ_CHANNEL = list(map(int, environ.get("AUTH_REQ_CHANNELS", "-1002220670718").split()))
 FSUB = str_to_bool(environ.get("FSUB", "True"))
-AUTH_PICS = environ.get("AUTH_PICS", "https://files.catbox.moe/facpku.jpg")
+AUTH_PICS = environ.get("AUTH_PICS", "https://files.catbox.moe/sg2tnu.jpg")
 CHANNEL = environ.get("CHANNEL", "https://t.me/DVITTALBOTZ")
 SUPPORT = environ.get("SUPPORT", "https://t.me/DVITTALBOTZ_DISSCUSSION")
 APP_URL = environ.get("APP_URL", "https://manual-nikolia-totzvvv-5115e05f.koyeb.app/")
